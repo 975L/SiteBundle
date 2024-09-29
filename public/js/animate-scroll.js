c9359847b3bb7227363fa0dfe4b90ac380e3c0f8
Laurent Marquet <laurent.marquet@laposte.net>
2024-09-29 18:51:05 +0200

    - Added {{ importmap('app') }} for asset-mapper (29/09/2024) - Moved block javascript to head (29/09/2024) - Converted javascripts to Stimulus controllers [BC-Break] (29/09/2024) - Added Component Stimulus:Controller (29/09/2024) - Added confetti animation (29/09/2024)

---
+++ public/js/animate-scroll.js
@@ -0,0 +1,37 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+    connect() {
+        this.animateOnScroll = this.animateOnScroll.bind(this)
+        window.addEventListener('scroll', this.animateOnScroll)
+        this.animateOnScroll();
+    }
+
+    disconnect() {
+        window.removeEventListener('scroll', this.animateOnScroll)
+    }
+
+    // Checks if element is in viewport
+    isElementInViewport(element, offset) {
+        if (null !== element) {
+            const rect = element.getBoundingClientRect();
+            return (
+                rect.top < (window.innerHeight || document.documentElement.clientHeight) - offset &&
+                rect.bottom >= 0
+            );
+        }
+        return false
+    }
+
+    // Animates on scroll
+    animateOnScroll() {
+        var elements = document.querySelectorAll('.scroll');
+        elements.forEach(element => {
+            if (this.isElementInViewport(element, 200)) {
+                const animationClass = element.getAttribute('data-animation')
+                element.classList.remove('hidden')
+                element.classList.add(animationClass)
+            }
+        })
+    }
+}
