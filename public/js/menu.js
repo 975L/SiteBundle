7d56059d9359a3aa8918dc13c3e747843cd59deb
Laurent Marquet <laurent.marquet@laposte.net>
2026-03-16 15:08:40 +0100

    - Added Menu and MenuItem (16/03/2026) - Added classLabel for some components (16/03/2026) - Moved default components value inside the code (16/03/2026)

---
+++ public/js/menu.js
@@ -0,0 +1,67 @@
+/**
+ * Menu Controller
+ * @author Laurent Marquet <laurent.marquet@laposte.net>
+ * @copyright 2026 975L <contact@975l.com>
+ */
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+    static targets = ["items"];
+
+    connect() {
+        // Close menu when clicking outside
+        this.boundHandleClickOutside = this.handleClickOutside.bind(this);
+        document.addEventListener("click", this.boundHandleClickOutside);
+
+        // Close menu on escape key
+        this.boundHandleEscape = this.handleEscape.bind(this);
+        document.addEventListener("keydown", this.boundHandleEscape);
+    }
+
+    disconnect() {
+        document.removeEventListener("click", this.boundHandleClickOutside);
+        document.removeEventListener("keydown", this.boundHandleEscape);
+    }
+
+    toggle(event) {
+        event.stopPropagation();
+        const button = event.currentTarget;
+        const isExpanded = button.getAttribute("aria-expanded") === "true";
+
+        if (isExpanded) {
+            this.close(button);
+        } else {
+            this.open(button);
+        }
+    }
+
+    open(button) {
+        this.itemsTarget.classList.add("open");
+        button.setAttribute("aria-expanded", "true");
+        button.classList.add("active");
+    }
+
+    close(button) {
+        this.itemsTarget.classList.remove("open");
+        button.setAttribute("aria-expanded", "false");
+        button.classList.remove("active");
+    }
+
+    handleClickOutside(event) {
+        if (!this.element.contains(event.target)) {
+            const button = this.element.querySelector(".menu-toggle");
+            if (button.getAttribute("aria-expanded") === "true") {
+                this.close(button);
+            }
+        }
+    }
+
+    handleEscape(event) {
+        if (event.key === "Escape") {
+            const button = this.element.querySelector(".menu-toggle");
+            if (button.getAttribute("aria-expanded") === "true") {
+                this.close(button);
+            }
+        }
+    }
+}
