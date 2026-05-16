91587faf6feb85b689fd1172a48f399e08a948e0
Laurent Marquet <laurent.marquet@laposte.net>
2026-05-16 23:02:39 +0200

    - Corrected javascript copyrights (16/05/2026)

--- public/js/cookie-consent.js
+++ public/js/cookie-consent.js
@@ -1,7 +1,9 @@
-/**
- * CookieConsent Controller
- * @author Laurent Marquet <laurent.marquet@laposte.net>
- * @copyright 2024 975L <contact@975l.com>
+/*
+ * (c) 2024: 975L <contact@975l.com>
+ * (c) 2024: Laurent Marquet <laurent.marquet@laposte.net>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
  */
 import { Controller } from "@hotwired/stimulus";
 
